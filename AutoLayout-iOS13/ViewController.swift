697a38dd4ec76a5a95853bbb5db423df5344ee5a
Widiyantoko <widiytk@gmail.com>
2021-06-20 19:54:59 +0800

    initial commit

---
+++ AutoLayout-iOS13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayout-iOS13
+//
+//  Created by gHost on 6/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
